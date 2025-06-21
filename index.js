12289a9216ff86dae26d12db069ecefb07eeac81
Shubham Karna <skarna230@gmail.com>
2025-06-21 23:36:11 +0530

    backend of henosis

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const PORT = 8000
 const express = require("express");
 const app = express();
 
+app.get( '/' , (req,res) => {
+    res.send("this is home page")
+})
+
+app.route('/login')
+.get((req,res) => {
+    res.send("this is login page")
+})
+
+
+
+
+app.get( '/signup' , (req,res) => {
+    res.send("this is signup page")
+})
+
+
 app.listen( PORT , () => {
     console.log(`Server started at http://localhost:${PORT}`);
 })
